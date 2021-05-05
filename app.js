b90e104afb25eaaa520524ac5ca94745e2eb0806
Alonzo A <68953196+Alonzo-Anderson@users.noreply.github.com>
2021-05-05 16:12:30 -0700

    added reset button

--- app.js
+++ app.js
@@ -6,12 +6,16 @@ const winDiv = document.querySelector('#total-wins');
 const lossDiv = document.querySelector('#total-losses');
 const results = document.querySelector('#current-result');
 const tiesDiv = document.querySelector('#total-ties');
+const resetDiv = document.querySelector('#total-reset');
+const btnR = document.querySelector('#reset-btn');
+
 console.log(tiesDiv);
 // initialize state
 const choice = ['rock','paper', 'scissors'];
 let wins = 0;
 let ties = 0;
 let totalGuess = 0;
+let resetsTot = 0;
 // set event listeners to update state and DOM
 btn.addEventListener('click', () => {
     //get right guess
@@ -30,10 +34,21 @@ btn.addEventListener('click', () => {
     } else{
         results.textContent = 'you guessed lost!';
     }
-    console.log(compChoice);
     totalGuess++;
     //update DOM
     winDiv.textContent = `You won this many times: ${wins}`;
     lossDiv.textContent = `You lost this many times: ${totalGuess - wins}`;
     tiesDiv.textContent = `You have tied this many times: ${ties}`;
+    resetDiv.textContent = `You have reset this many times: ${resetsTot}`;
+})
+
+btnR.addEventListener('click', () =>{
+    totalGuess = 0;
+    ties = 0;
+    wins = 0;
+    resetsTot++;
+    winDiv.textContent = `You won this many times: ${wins}`;
+    lossDiv.textContent = `You lost this many times: ${totalGuess - wins}`;
+    tiesDiv.textContent = `You have tied this many times: ${ties}`;
+    resetDiv.textContent = `You have reset this many times: ${resetsTot}`;
 })
